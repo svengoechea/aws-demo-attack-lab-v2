bfac1cb3c979f0038ddfc651f8f18c31cad8f70e
David Avila <33632789+davidaavilar@users.noreply.github.com>
2023-12-21 18:13:25 -0500

    modified files

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "__backendAWSBucketName__"
-    key    = "__backendAWSKey__"
-    region = "__aws_Region__"
+    bucket = ""
+    key    = ""
+    region = ""
   }
 }
