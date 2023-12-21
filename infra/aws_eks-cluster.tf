828dcce8c045dad0fbe3df74f2fe93bb559ba75f
David Avila <33632789+davidaavilar@users.noreply.github.com>
2023-12-21 18:19:10 -0500

    modified files

--- infra/aws_eks-cluster.tf
+++ infra/aws_eks-cluster.tf
@@ -1,46 +1,46 @@
-# locals {
-#   cmd_update_kube_config = "aws eks update-kubeconfig --name ${module.eks.cluster_name} --region ${var.region}"
-# }
-# ## Updating the local Kubeconfig
-# resource "null_resource" "update-kube-config" {
-#   provisioner "local-exec" {
-#     command = local.cmd_update_kube_config
-#   }
-#   depends_on = [
-#     module.eks
-#   ]
-# }
+locals {
+  cmd_update_kube_config = "aws eks update-kubeconfig --name ${module.eks.cluster_name} --region ${var.region}"
+}
+## Updating the local Kubeconfig
+resource "null_resource" "update-kube-config" {
+  provisioner "local-exec" {
+    command = local.cmd_update_kube_config
+  }
+  depends_on = [
+    module.eks
+  ]
+}
 
-# module "eks" {
-#   source                         = "terraform-aws-modules/eks/aws"
-#   version                        = "19.0.4"
-#   cluster_name                   = "${var.deployment_name}-${random_string.unique_id.result}"
-#   cluster_version                = "1.27"
-#   vpc_id                         = module.vpc.vpc_id
-#   subnet_ids                     = module.vpc.private_subnets
-#   cluster_endpoint_public_access = true
-#   eks_managed_node_group_defaults = {
-#     ami_type = "AL2_x86_64"
+module "eks" {
+  source                         = "terraform-aws-modules/eks/aws"
+  version                        = "19.0.4"
+  cluster_name                   = "${var.deployment_name}-${random_string.unique_id.result}"
+  cluster_version                = "1.27"
+  vpc_id                         = module.vpc.vpc_id
+  subnet_ids                     = module.vpc.private_subnets
+  cluster_endpoint_public_access = true
+  eks_managed_node_group_defaults = {
+    ami_type = "AL2_x86_64"
 
-#   }
-#   eks_managed_node_groups = {
-#     one = {
-#       name = "${var.deployment_name}-ng1"
+  }
+  eks_managed_node_groups = {
+    one = {
+      name = "${var.deployment_name}-ng1"
 
-#       instance_types = ["t3.small"]
+      instance_types = ["t3.small"]
 
-#       min_size     = 1
-#       max_size     = 4
-#       desired_size = 2
-#     }
-#     two = {
-#       name = "${var.deployment_name}-ng2"
+      min_size     = 1
+      max_size     = 4
+      desired_size = 2
+    }
+    two = {
+      name = "${var.deployment_name}-ng2"
 
-#       instance_types = ["t3.small"]
+      instance_types = ["t3.small"]
 
-#       min_size     = 0
-#       max_size     = 2
-#       desired_size = 0
-#     }
-#   }
-# }
+      min_size     = 0
+      max_size     = 2
+      desired_size = 0
+    }
+  }
+}
--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,15 +1,15 @@
 
-# output "cluster_name" {
-#   description = "Amazon Web Service EKS Cluster Name"
-#   value       = module.eks.cluster_name
-# }
+output "cluster_name" {
+  description = "Amazon Web Service EKS Cluster Name"
+  value       = module.eks.cluster_name
+}
 
-# output "cluster_endpoint" {
-#   description = "Endpoint for Amazon Web Service EKS "
-#   value       = module.eks.cluster_endpoint
-# }
+output "cluster_endpoint" {
+  description = "Endpoint for Amazon Web Service EKS "
+  value       = module.eks.cluster_endpoint
+}
 
-# output "update_kube_config" {
-#   description = "Update your Kube Config to connect to your cluster (optional)"
-#   value       = "aws eks update-kubeconfig --name ${module.eks.cluster_name} --region ${var.region}"
-# }
+output "update_kube_config" {
+  description = "Update your Kube Config to connect to your cluster (optional)"
+  value       = "aws eks update-kubeconfig --name ${module.eks.cluster_name} --region ${var.region}"
+}
