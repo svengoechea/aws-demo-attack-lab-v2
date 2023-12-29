c68aaa4e683ba11b884f7fd3450b67e55acd3029
David Avila <33632789+davidaavilar@users.noreply.github.com>
2023-12-29 09:12:20 -0500

    updated

--- infra/aws_lambda.tf
+++ infra/aws_lambda.tf
@@ -32,7 +32,7 @@ resource "aws_lambda_function" "analysis_lambda" {
     variables = {
       access_key = "AKIAIOSFODNN7EXAMPLE"
       secret_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
-      test = "testing2"
+      test = "testing3"
     }
   }
 }
