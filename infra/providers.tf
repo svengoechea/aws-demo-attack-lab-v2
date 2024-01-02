5fd334e620f934d21dbb9e86ffb88c469c840601
David Avila <33632789+davidaavilar@users.noreply.github.com>
2024-01-02 11:48:49 -0500

    updated files

--- infra/providers.tf
+++ infra/providers.tf
@@ -35,9 +35,6 @@ terraform {
 provider "aws" {
   region = var.region
 }
-provider "kubernetes" {
-  config_path = "~/.kube/config"
-}
 
 provider "docker" {}
 
@@ -46,19 +43,4 @@ provider "random" {}
 provider "kubectl" {}
 
 provider "time" {}
-
-### If you want to connect to K8s Cluster using EKS (not necessary)
-# data "aws_eks_cluster" "cluster" {
-#   name = module.eks.cluster_name
-# }
-
-# data "aws_eks_cluster_auth" "cluster" {
-#   name = module.eks.cluster_name
-# }
-
-# provider "kubernetes" {
-#   host                   = data.aws_eks_cluster.cluster.endpoint
-#   token                  = data.aws_eks_cluster_auth.cluster.token
-#   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-# }
 
