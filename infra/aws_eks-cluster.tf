84a96961bd0c38a79cd6ce65f9c6e22706761c18
David Avila <33632789+davidaavilar@users.noreply.github.com>
2024-01-09 08:40:12 -0500

    updated eks cluster

--- infra/aws_eks-cluster.tf
+++ infra/aws_eks-cluster.tf
@@ -1,33 +1,33 @@
-# module "eks" {
-#   source                         = "terraform-aws-modules/eks/aws"
-#   version                        = "19.0.4"
-#   cluster_name                   = "${var.deployment_name}-${random_string.unique_id.result}"
-#   cluster_version                = "1.27"
-#   vpc_id                         = module.vpc.vpc_id
-#   subnet_ids                     = module.vpc.private_subnets
-#   cluster_endpoint_public_access = true
-#   eks_managed_node_group_defaults = {
-#     ami_type = "AL2_x86_64"
+module "eks" {
+  source                         = "terraform-aws-modules/eks/aws"
+  version                        = "19.0.4"
+  cluster_name                   = "${var.deployment_name}-${random_string.unique_id.result}"
+  cluster_version                = "1.28"
+  vpc_id                         = module.vpc.vpc_id
+  subnet_ids                     = module.vpc.private_subnets
+  cluster_endpoint_public_access = true
+  eks_managed_node_group_defaults = {
+    ami_type = "AL2_x86_64"
 
-#   }
-#   eks_managed_node_groups = {
-#     one = {
-#       name = "${var.deployment_name}-ng1"
+  }
+  eks_managed_node_groups = {
+    one = {
+      name = "${var.deployment_name}-ng1"
 
-#       instance_types = ["t3.medium"]
+      instance_types = ["t3.small"]
 
-#       min_size     = 1
-#       max_size     = 5
-#       desired_size = 2
-#     }
-#     two = {
-#       name = "${var.deployment_name}-ng2"
+      min_size     = 1
+      max_size     = 4
+      desired_size = 2
+    }
+    # two = {
+    #   name = "${var.deployment_name}-ng2"
 
-#       instance_types = ["t3.medium"]
+    #   instance_types = ["t3.medium"]
 
-#       min_size     = 0
-#       max_size     = 2
-#       desired_size = 0
-#     }
-#   }
-# }
+    #   min_size     = 0
+    #   max_size     = 2
+    #   desired_size = 0
+    # }
+  }
+}
