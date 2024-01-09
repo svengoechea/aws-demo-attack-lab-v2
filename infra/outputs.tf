cafaaaa6c8edbecfbae0963fa8d97d5389d642d0
David Avila <33632789+davidaavilar@users.noreply.github.com>
2024-01-09 08:28:48 -0500

    Update outputs.tf

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,20 +1,20 @@
 
-output "cluster_name" {
-  description = "Amazon Web Service EKS Cluster Name"
-  value       = module.eks.cluster_name
-}
+# output "cluster_name" {
+#   description = "Amazon Web Service EKS Cluster Name"
+#   value       = module.eks.cluster_name
+# }
 
-output "region" {
-  description = "AWS Region"
-  value       = var.region
-}
+# output "region" {
+#   description = "AWS Region"
+#   value       = var.region
+# }
 
-output "cluster_endpoint" {
-  description = "Endpoint for Amazon Web Service EKS "
-  value       = module.eks.cluster_endpoint
-}
+# output "cluster_endpoint" {
+#   description = "Endpoint for Amazon Web Service EKS "
+#   value       = module.eks.cluster_endpoint
+# }
 
-output "update_kube_config" {
-  description = "Update your Kube Config to connect to your cluster (optional)"
-  value       = "aws eks update-kubeconfig --name ${module.eks.cluster_name} --region ${var.region}"
-}
+# output "update_kube_config" {
+#   description = "Update your Kube Config to connect to your cluster (optional)"
+#   value       = "aws eks update-kubeconfig --name ${module.eks.cluster_name} --region ${var.region}"
+# }
