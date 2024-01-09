025a77801f1f400dc00f1587594e8600e47b3a29
David Avila <33632789+davidaavilar@users.noreply.github.com>
2024-01-09 08:45:58 -0500

    Update providers.tf

--- infra/providers.tf
+++ infra/providers.tf
@@ -1,9 +1,5 @@
 terraform {
   required_providers {
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = "2.22.0"
-    }
     aws = {
       source  = "hashicorp/aws"
       version = "~> 4.46.0"
@@ -20,14 +16,6 @@ terraform {
       source  = "hashicorp/cloudinit"
       version = "~> 2.2.0"
     }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.16.1"
-    }
-    kubectl = {
-      source  = "gavinbunney/kubectl"
-      version = "1.14.0"
-    }
   }
   required_version = "~> 1.3"
 }
@@ -36,11 +24,7 @@ provider "aws" {
   region = var.region
 }
 
-provider "docker" {}
-
 provider "random" {}
-
-provider "kubectl" {}
 
 provider "time" {}
 
