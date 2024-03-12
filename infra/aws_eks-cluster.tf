bd70ec50e91ebadd3be2d15d920df9493898aeaf
David Avila <33632789+davidaavilar@users.noreply.github.com>
2024-03-12 13:41:01 -0500

    updated files

--- infra/aws_eks-cluster.tf
+++ infra/aws_eks-cluster.tf
@@ -2,7 +2,7 @@ module "eks" {
   source                         = "terraform-aws-modules/eks/aws"
   version                        = "19.0.4"
   cluster_name                   = "${var.deployment_name}-${random_string.unique_id.result}"
-  cluster_version                = "1.28"
+  cluster_version                = "1.25"
   vpc_id                         = module.vpc.vpc_id
   subnet_ids                     = module.vpc.private_subnets
   cluster_endpoint_public_access = true
